cfa05d55c065adf6d0792e35da1186d196950489
Marsk27 <marskirova@gmail.com>
2022-10-29 12:16:51 +0200

    new

---
+++ javascript/test.js
@@ -0,0 +1,17 @@
+var a =10 
+//var : variabels : it is the memory location where we can store value.
+// 10+20 : value and 20 value and result as will.
+// memory +
+var b = 20;
+
+a ="mars";
+b= true; // true ; yes, false : no : boolean value 
+
+var c = a+b;
+
+console.log("hello from mars");
+console.log(" result is"+ c); 
+// js we have to use typescript: it is the wrapper on javascript.
+
+// strict type checking is followed by typescript.
+//  
